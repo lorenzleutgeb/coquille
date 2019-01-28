cfca81c023c46e8a45a9c982df6060d0e1c58e49
LEPILLER Julien <julien.lepiller@irisa.fr>
2019-01-28 14:22:29 +0100

    Fix tests for changes in projectparser

--- rplugin/python3/pycoqtop/test_parser.py
+++ rplugin/python3/pycoqtop/test_parser.py
@@ -3,6 +3,9 @@ from .projectparser import ProjectParser
 def test_notfound():
     try:
         parser = ProjectParser("test_data/NonExistingFile")
+        assert not parser.getI()
+        assert not parser.getQ()
+        assert len(parser.getR()) == 1
     except FileNotFoundError:
         pass
 
@@ -10,11 +13,11 @@ def test_empty():
     parser = ProjectParser("test_data/emptyCoqProject")
     assert not parser.getI()
     assert not parser.getQ()
-    assert len(parser.getR()) == 1
+    assert len(parser.getR()) == 0
 
 def test_some():
     parser = ProjectParser("test_data/_CoqProject")
     assert not parser.getQ()
     assert len(parser.getI()) == 1
     assert parser.getI()[0] == "/usr/local/lib"
-    assert len(parser.getR()) == 12
+    assert len(parser.getR()) == 11
