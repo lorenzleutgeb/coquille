174ce3b202a19ae33883620a17d7a534be283e49
LEPILLER Julien <julien.lepiller@irisa.fr>
2019-05-14 15:41:51 +0200

    Add tests for 8.6 back

--- rplugin/python3/pycoqtop/test.sh
+++ rplugin/python3/pycoqtop/test.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-# TODO: Add 8.6
-for version in 8.7 8.8 8.9; do
+for version in 8.6 8.7 8.8 8.9; do
     if guix environment --ad-hoc python-pytest coq@${version} -- py.test; then
         printf "Success with coq ${version}\n"
     else
